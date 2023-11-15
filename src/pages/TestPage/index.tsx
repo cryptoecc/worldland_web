2b8d11f57a8ca143d564b344a8602714ef9d4adc
J-Chan96 <juchan1996@naver.com>
2023-11-15 12:08:03 +0900

    testpage solved

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -11,6 +11,8 @@ import { web3_eth } from 'configs/web3-eth';
 import { web3_wld } from 'configs/web3-wld';
 import { MAPNETTOADDRESS } from 'configs/contract_address_config';
 import { useToasts } from 'react-toast-notifications';
+import { useAccount, useContractWrite, useContractRead, useContractReads } from 'wagmi';
+import { gasLimit } from 'utils/wagmi';
 
 const Container = styled.section`
   display: flex;
@@ -54,6 +56,7 @@ const StyledButton = styled.button`
 `;
 
 const TestPage = () => {
+  const { address } = useAccount();
   const { addToast } = useToasts();
   const [inputValues, setInputValues] = useState(Array(10).fill(''));
   const [priceValues, setPriceValues] = useState(Array(10).fill(''));
@@ -63,7 +66,7 @@ const TestPage = () => {
   const [loading, setLoading] = useState(false);
   const [priceList, setPriceList] = useState<string[]>([]);
   const [predPrice, setPredPrice] = useState();
-  const [account, setAccount] = useState<string | null>(null);
+  // const [account, setAccount] = useState();
   const [timestamp, setTimestamp] = useState('');
   const [isButtonVisible, setIsButtonVisible] = useState<boolean>(false);
 
@@ -82,8 +85,7 @@ const TestPage = () => {
         const accounts = await web3.eth.getAccounts();
         if (accounts.length > 0) {
           const userAddress = accounts[0];
-          setAccount(userAddress);
-          // console.log(`Metamask 연결 성공. 사용자 주소: ${userAddress}`);
+          // setAccount(userAddress);
           return web3; // Metamask와 연결된 Web3 인스턴스 반환
         } else {
           console.error('Metamask로부터 사용자 주소를 가져올 수 없습니다.');
@@ -103,22 +105,7 @@ const TestPage = () => {
     setInputValues([...priceList.slice(0, 10)]);
   };
 
-  // 렌더링 시 chainlink에서 가격 가져오기
-  useEffect(() => {
-    setLoading(true);
-    getData();
-    connectToMetamask();
-    let sepolia_web3 = web3_eth; //testnet
-    setWeb3(sepolia_web3);
-    if (sepolia_web3) {
-      try {
-        setAccount(account);
-      } catch (error) {
-        console.error('메타마스크 연결에 실패 : ', error);
-      }
-    }
-  }, [web3]);
-  console.log(account);
+  // console.log(account);
   const getData = async () => {
     //(1) 가격 가져오기
     if (web3) {
@@ -143,21 +130,6 @@ const TestPage = () => {
         const answer = new BigNumber(await (contract.methods.getAnswer as any)(roundID_str).call());
         const BIG_TEN = new BigNumber(10);
         const answer_Big = answer.dividedBy(BIG_TEN.pow(8)).toFixed(2); // 10^8로 나누고, 소수점 두 자리까지
-        const answer_str = answer_Big.toString();
-        // console.log('언제냐 getAnswer :', answer_str);
-
-        //getTimestamp 호출
-        const timestamp = new BigNumber(await (contract.methods.getTimestamp as any)(roundID_str).call());
-        const date = new Date(timestamp.toNumber() * 1000); //1000곱해서 밀리초로 변환
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        const formattedDate = `${year}년 ${month}월 ${day}일 ${hours}:${minutes}:${seconds}`;
-
-        setTimestamp(formattedDate);
 
         //10개의 roundId에 대한 가격 데이터
         const tempPriceList: string[] = [];
@@ -171,7 +143,6 @@ const TestPage = () => {
             console.error(`getAnswer 호출 중 오류 발생한 roundID ${roundIDList[i]}:`, error);
           }
         }
-        // console.log('templist다', tempPriceList);
         setPriceList(tempPriceList);
 
         setLoading(false);
@@ -183,7 +154,6 @@ const TestPage = () => {
 
   //(2) 예측모델로 가격 정보 보내고, 예측값 가져오기
   const fetchData = async () => {
-    // console.log(priceList);
     let url = 'https://aim.worldland.foundation/?';
 
     //priceList의 10개의 값을 쿼리 파라미터로 추가
@@ -193,15 +163,11 @@ const TestPage = () => {
       });
     }
 
-    // console.log(url);
     //fetch로 GET요청
     try {
       const response = await fetch(url);
-      // console.log(response);
       const data = await response.json();
-      // console.log(data);
       if (data) {
-        // console.log('123');
         const tempPredList = data.prediction;
         setPriceValues(tempPredList);
 
@@ -214,82 +180,50 @@ const TestPage = () => {
     }
   };
   const sendTransaction = async (priceValues: any) => {
-    console.log('@먼 값임?', priceValues);
     let worldland_web3 = web3_wld;
-    // const accounts = await worldland_web3.eth.getAccounts();
-    // console.log(accounts);
-    // setAccount(accounts[0]);
-    // await localStorage.setItem('connectedAccount', accounts[0]);
 
-    // console.log(worldland_web3, '?');
-    // 왜 빈배열이지
-    // console.log(await worldland_web3.eth.getBlockNumber());
     if (worldland_web3) {
-      console.log('@이건 또 먼 값임??', priceValues);
-
       const contract = new worldland_web3.eth.Contract(
         MAP_STR_ABI[ABI.LVSWAPV2_ROUTER],
         MAPNETTOADDRESS[CONTRACT_ADDRESSES.ROUTER],
       );
-      // console.log(contract, '보자');
-      // const accounts = await worldland_web3.eth.getAccounts();
-      // console.log(accounts);
-      // const account = accounts[0]; // 첫 번째 계정을 가져올 수 있습니다.
-      // const account = '0xd67c40b6488032BAC40C781c52E55e2593345689';
-      setAccount(account as string);
-      // console.log(account, '@ array value');
-      // console.log('현재 연결된 계정:', account);
-      // const account = await web3.eth.getAccounts();
 
-      // const token0 = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'; // Router 배포할 때 WETH address
       const token0 = WLD_ADDRESSES[CONTRACT_ADDRESSES.ETH_TOKEN_ADDRESS]; // eth token address
       const token1 = WLD_ADDRESSES[CONTRACT_ADDRESSES.DAI_TOKEN_ADDRESS];
       const BlockNumber = await worldland_web3.eth.getBlockNumber();
-      // const a = [1796.81, 1796.83, 1796.79, 1796.77, 1796.77, 1796.79, 1796.8, 1796.83, 1796.81, 1796.84];
+
       let PredictedPrice = [];
       for (let i = 0; i < 10; i++) {
         PredictedPrice.push(worldland_web3.utils.toWei(Number([priceList[i]]), 'ether'));
       }
-      console.log('2', PredictedPrice);
-      let nonce = await worldland_web3.eth.getTransactionCount(account as string);
-      // console.log({ nonce });
-      // const txObject = {
-      //   from: account,
-      //   to: MAPNETTOADDRESS[CONTRACT_ADDRESSES.ROUTER],
-      //   data: (contract.methods.setMarketPricesAtPool as any)(token0, token1, BlockNumber, PredictedPrice).encodeABI(),
-      //   gasPrice: '100000000000',
-      //   gas: 3000000,
-      //   nonce: nonce,
-      // };
-      const txObject: TransactionWithFromAndToLocalWalletIndex = {
-        from: account as string,
-        to: MAPNETTOADDRESS[CONTRACT_ADDRESSES.ROUTER],
-        data: (contract.methods.setMarketPricesAtPool as any)(token0, token1, BlockNumber, PredictedPrice).encodeABI(),
-        gasPrice: '100000000000',
-        gas: 3000000,
-        nonce: nonce,
-      };
-      console.log('이걸 보낸다고?', txObject);
-      console.log('@이값이 찍히니?', contract.methods.setMarketPricesAtPool as any);
-      // console.log(
-      //   'tx오브젝트',
-      //   (contract.methods.setMarketPricesAtPool as any)(token0, token1, BlockNumber, PredictedPrice),
-      // );
-      // console.log('???', PredictedPrice);
 
       try {
-        const result = await worldland_web3.eth.sendTransaction(txObject);
-        console.log('트랜잭션 결과:', result);
+        setMarketPrice({
+          args: [token0, token1, BlockNumber, PredictedPrice],
+        });
       } catch (error: any) {
         console.error('트랜잭션을 보내는 중에 오류 발생:', error);
-        // window.alert('트랜잭션을 보내는 중에 오류 발생: ' + error.message);
-        // addToast('트랜잭션 처리 중 오류가 발생했습니다.', {
-        //   appearance: 'error', // 오류 메시지 스타일
-        //   autoDismiss: true, // 자동 닫기
-        // });
       }
     }
   };
+
+  const { write: setMarketPrice } = useContractWrite({
+    address: MAPNETTOADDRESS[CONTRACT_ADDRESSES.ROUTER],
+    abi: MAP_STR_ABI[ABI.LVSWAPV2_ROUTER],
+    functionName: 'setMarketPricesAtPool',
+    gas: gasLimit,
+    onSuccess(data) {
+      console.log({ setMarketPrice: data });
+    },
+    onError(err) {
+      window.alert('트랜잭션을 보내는 중에 오류 발생: ' + err.message.substring(0, 103));
+      addToast('트랜잭션 처리 중 오류가 발생했습니다.', {
+        appearance: 'error', // 오류 메시지 스타일
+        autoDismiss: true, // 자동 닫기
+      });
+      console.log({ setMarketPrice: err });
+    },
+  });
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
     const value = e.target.value;
@@ -308,6 +242,22 @@ const TestPage = () => {
       console.log('error');
     }
   };
+
+  // 렌더링 시 chainlink에서 가격 가져오기
+  useEffect(() => {
+    setLoading(true);
+    getData();
+    connectToMetamask();
+    let sepolia_web3 = web3_eth; //testnet
+    setWeb3(sepolia_web3);
+    if (sepolia_web3) {
+      try {
+        // setAccount(account);
+      } catch (error) {
+        console.error('메타마스크 연결에 실패 : ', error);
+      }
+    }
+  }, [web3]);
 
   return (
     <Container>
